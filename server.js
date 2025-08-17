80443633248b9e06a7f757f58d7d81c0e85330a9
Gayatri217003 <217003.gjk@gmail.com>
2025-08-18 03:32:32 +0530

    Update server.js

--- server.js
+++ server.js
@@ -29,5 +29,5 @@ app.use((err, req, res, next) => {
   res.status(500).json({ message: "Internal server error" });
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
